2dd8a366d7c82cc1434d3bd98713b0617e45d513
Denny Huynh <customtagsco@gmail.com>
2016-08-04 15:54:00 -0400

    Latest Update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,5 +78,5 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # Required for Heroku
-  config.action_mailer.default_url_options = { host: => 'highriseboston.com' }
+  config.action_mailer.default_url_options = { host: => 'http;//firstrailspage.herokuapp.com' }
 end
